1d9a7cdbf8fc6e049f594b7759a111613fcfad32
Himanshujha2106 <himanshu.jha@ssipmt.com>
2024-04-23 10:53:23 +0530

    22/04/2024

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,2 @@
-/usr/local/bin/pip install psycopg2-binary
-# pip install -r requirements.txt
-/usr/local/bin/pip install -r requirements.txt
+pip install psycopg2-binary
+pip install -r requirements.txt
